12d38e5037124f64fec26396780f58e1e0f8d8ef
nikit91@gmail.com <nikit91@gmail.com>
2018-06-06 18:22:01 +0200

    Implementing default message

--- src/main/java/upb/ida/rest/MessageRestController.java
+++ src/main/java/upb/ida/rest/MessageRestController.java
@@ -7,10 +7,16 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/message")
 public class MessageRestController {
+
+	@RequestMapping("/sayhello")
+	public String greeting(@RequestParam(value = "name", defaultValue = "World") String name) {
+		return "Hello " + name + "!";
+	}
 	
-	 @RequestMapping("/sayhello")
-	    public String greeting(@RequestParam(value="name", defaultValue="World") String name) {
-	        return "Hello "+name+"!";
-	    }
+	@RequestMapping("/sendmessage")
+	public String sendmessage(@RequestParam(value = "msg") String msg) {
+		System.out.println(msg);
+		return "Service under development. Please try later.";
+	}
 
 }
