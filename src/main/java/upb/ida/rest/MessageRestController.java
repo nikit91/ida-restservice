587a03d771f9c60eff1daa7c5d26df2a0920c136
nikit91@gmail.com <nikit91@gmail.com>
2018-06-06 18:12:32 +0200

    Working Resful Service

---
+++ src/main/java/upb/ida/rest/MessageRestController.java
@@ -0,0 +1,16 @@
+package upb.ida.rest;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/message")
+public class MessageRestController {
+	
+	 @RequestMapping("/sayhello")
+	    public String greeting(@RequestParam(value="name", defaultValue="World") String name) {
+	        return "Hello "+name+"!";
+	    }
+
+}
